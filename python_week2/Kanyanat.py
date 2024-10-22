a70ff47bf3eefbe3c47dacbda965dc8cfb5c3fda
Kanyanat-tnk <67319010042@tnk.ac.th>
2024-10-22 09:05:59 +0700

    Introduce myself
    week2

---
+++ python_week2/Kanyanat.py
@@ -0,0 +1,7 @@
+# คือคอมเม้น print คือคำสั่งแสดง, \t คือย่อหน้า \n คือขึ้นบรรทัดใหม่
+print('introduce myself')
+print('Kanyanat Luethip')
+print('Film')
+print('No.1')
+print('67319010042')
+print('play computer games and play the guitar')
