e78ed5a075a583d33cffd2858b8e5997752f070b
Kanyanat-tnk <67319010042@tnk.ac.th>
2024-10-22 13:44:09 +0700

    grade

--- python_week2/grade.py
+++ python_week2/grade.py
@@ -6,10 +6,12 @@ elif score >= 80 :
     print('เกรด 4')
 elif score >= 70 :
     print('เกรด 3')
-elif score >= 50 :
+elif score >= 60 :
     print('เกรด 2')
-elif score < 50 :
+elif score >=50 :
     print('เกรด 1')
+elif score <50 :
+    print('เกรด 0')
 else :
     print('ป้อนค่าไม่ถูกต้อง')
 
