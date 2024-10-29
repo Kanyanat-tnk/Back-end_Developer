19cf0574af9935f437f6555b13ca299e63bf0bb7
Kanyanat-tnk <67319010042@tnk.ac.th>
2024-10-29 13:32:20 +0700

    Rock-Paper-Scissors

---
+++ python_week3/RPSrandomchoice.py
@@ -0,0 +1,15 @@
+import random
+print('โปรแกรมเป่ายิงฉุบ')
+while True : 
+    a = random.choice(["ค้อน","กรรไกร","กระดาษ"])
+    print(a)
+    b = str(input('ระบุคำตอบ'))
+    if b == a :
+        print('เสมอ')
+    elif a =="กรรไกร" and b =="ค้อน":
+        print('คุณชนะ')
+    elif a =="กระดาษ" and b =="กรรไกร":
+        print('คุณชนะ')
+    elif a =="ค้อน" and b =="กระดาษ":
+        print('คุณชนะ')
+        
